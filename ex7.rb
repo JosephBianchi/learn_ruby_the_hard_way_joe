ddf30014b11986f8b635e162d35de807103a9e67
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-28 20:44:32 -0400

    fin. up to ex10

---
+++ ex7.rb
@@ -0,0 +1,23 @@
+puts "Mary had a little lamb"
+puts "its fleexe was white as #{'snow'}"
+puts "and everywehre that Mary went"
+puts "." * 10 # what'd that do?
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "s"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+
+# watch that rpint vs. puts on this line wha's it do?
+
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 +end11 + end12
