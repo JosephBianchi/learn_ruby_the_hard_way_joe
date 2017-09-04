e4171c7e3078eabaec90712ba90c57e6a65056b2
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-09-04 14:40:42 -0400

    ex28 done

---
+++ ex28.rb
@@ -0,0 +1,35 @@
+p true && true #true
+
+p false && true #false
+
+p 1 == 1 && 2 == 1 #false
+
+p "test" == "test" #true
+
+p 1 == 1 || 2 != 1 #true
+
+p true && 1 == 1 #true
+
+p "test" == "testing" #false
+
+p 1 != 0 && 2 == 1 #false
+
+p "test" != "testing" #true
+
+p "test" == 1 #false
+
+p !(true && false) #true
+
+p !(1 == 1 && 0 != 1) #false
+
+p !(10 == 1 || 1000 == 1000) #false
+
+p !(1 != 10 || 3 == 4) #false
+
+p !("testing" == "testing" && "zed" == "cool guy") #true
+
+p 1 == 1 && (!("testing" == 1 || 1 == 0)) #true
+
+p "chunky" == "bacon" && (!(3 == 4 || 3 == 3)) #false
+
+p 3 == 3 && (!("testing" == "testing" || "ruby" == "fun")) #false
